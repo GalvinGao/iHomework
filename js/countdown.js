ae8c7071463cf4901c79991fa4c8cfc9a92a2418
Galvin Gao <galvingaoedu@icloud.com>
2016-09-06 18:19:21 +0800

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,35 +1,40 @@
-function getRTime(){ 
-var EndTime= new Date('2016/9/9 17:00:00'); //截止时间 
-var NowTime = new Date();
-var t =EndTime.getTime() - NowTime.getTime(); 
-/*var d=Math.floor(t/1000/60/60/24); 
+function getRTime() {
+    var EndTime = new Date('2016/9/9 17:00:00');
+    //截止时间 
+    var NowTime = new Date();
+    var t = EndTime.getTime() - NowTime.getTime();
+    /*var d=Math.floor(t/1000/60/60/24); 
 t-=d*(1000*60*60*24); 
 var h=Math.floor(t/1000/60/60); 
 t-=h*60*60*1000; 
 var m=Math.floor(t/1000/60); 
 t-=m*60*1000; 
-var s=Math.floor(t/1000);*/ 
+var s=Math.floor(t/1000);*/
 
-var d=Math.floor(t/1000/60/60/24); 
-var h=Math.floor(t/1000/60/60%24); 
-var m=Math.floor(t/1000/60%60); 
-var s=Math.floor(t/1000%60); 
+var d = Math.floor(t / 1000 / 60 / 60 / 24);
+    var h = Math.floor(t / 1000 / 60 / 60 % 24);
+    var m = Math.floor(t / 1000 / 60 % 60);
+    var s = Math.floor(t / 1000 % 60);
 
-document.getElementById("t_d").innerHTML = d + "天"; 
-document.getElementById("t_h").innerHTML = h + "时"; 
-document.getElementById("t_m").innerHTML = m + "分"; 
-document.getElementById("t_s").innerHTML = s + "秒"; 
+    document.getElementById("t_d").innerHTML = d + "天";
+    document.getElementById("t_h").innerHTML = h + "时";
+    document.getElementById("t_m").innerHTML = m + "分";
+    document.getElementById("t_s").innerHTML = s + "秒";
 
-if (d==0 && h==0 && m==0 && s==0) 
-   {
-   document.getElementById("autolink").innerHTML = "d";
-   }
-else if (s<0)
-   {
-   document.getElementById("autolink").innerHTML = "<a target=\"_black\" class=\"waves-effect waves-light btn-large modal-trigger\" href=\"http://ihomeworkkhs.applinzi.com/ihomework_cdn.php\"><i class=\"material-icons left\">cloud</i>点我到 iHomework<script type=\"text/javascript\">mixpanel.track(\"CDN Redirect\");</script></a>"; 
-   }
-else
-   {
-   document.getElementById("autolink").innerHTML = "<a class=\"btn-large disabled\"><i class=\"material-icons left\">cloud</i>iHomework 于暑假阶段不可用</a>";
-   }
+    if (d == 0 && h == 0 && m == 0 && s == 0)
+    {
+        document.getElementById("autolink").innerHTML = "d";
+
+    }
+    else if (s < 0)
+    {
+        document.getElementById("autolink").innerHTML = "f";
+
+    }
+    else
+    {
+        document.getElementById("autolink").innerHTML = "g";
+
+    }
+
 }
