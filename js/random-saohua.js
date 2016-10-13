1848d9519223d841f264ed46974e0404253ba0d5
Galvin Gao <galvingaoedu@icloud.com>
2016-10-13 18:30:00 +0800

    Update random-saohua.js

--- js/random-saohua.js
+++ js/random-saohua.js
@@ -1,5 +1,5 @@
 window.onload = function(){ 
-var arr = ["要比勤礼拖堂长，<br><br>一节更比一节强。","傻子 (zǐ)"]; 
+var arr = ["要比勤礼拖堂长，<br><br>一节更比一节强。","博 zǐ<br>笑 zǐ<br>(老)师 zǐ<br>电(视) zǐ<br>脑 zǐ<br>骚 zǐ<br>踢 zǐ<br>（闪现）沙县 zǐ<br>"]; 
 
 var index = Math.floor((Math.random()*arr.length)); 
 document.getElementById("saohua").innerHTML = arr[index]; 
