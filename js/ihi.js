312a205f87a88ec4063c243c32f458a4b54a3e5c
GalvinGao <2403901511@qq.com>
2016-12-04 18:28:53 +0800

    123

---
+++ js/ihi.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    
+//$.get("http://api.github.com/repos/GalvinGao/iHomework/commits", function(data){document.getElementById("ihi-commitsha").innerHTML=JSON.stringify(data[0].sha);});
+
+var ihiver = "v4.1.0-alpha.4";
+var ihiupdate = "2016-12-4 17:23 GMT+8";
+
+document.getElementById("ihi-version").innerHTML = ihiver;
+document.getElementById("ihi-update").innerHTML = ihiupdate;
+//document.getElementById("ihi-commitsha").innerHTML = ihisha;
+    
+}
