6ed6f84d51cea2128f96f21f368a3bc1c29a35e5
GalvinG <2403901511@qq.com>
2016-12-06 11:24:36 +0800

    .

--- js/ihi.js
+++ js/ihi.js
@@ -3,7 +3,7 @@ window.onload = function() {
 //$.get("http://api.github.com/repos/GalvinGao/iHomework/commits", function(data){document.getElementById("ihi-commitsha").innerHTML=JSON.stringify(data[0].sha);});
 
 var ihiver = "v4.1.0-alpha.4";
-var ihiupdate = "2016-12-4 17:23 GMT+8";
+var ihiupdate = "2016-12-6 12:28 GMT+8";
 
 document.getElementById("ihi-version").innerHTML = ihiver;
 document.getElementById("ihi-update").innerHTML = ihiupdate;
