0722d99c7406fd0aabe2f6d7f30d3fbf5e77e582
GalvinG <2403901511@qq.com>
2016-12-28 15:01:39 +0800

    GG!

--- js/saoimg.js
+++ js/saoimg.js
@@ -2,10 +2,15 @@ function saoimg() {
     document.getElementById("saoimg").innerHTML = "";
 
 	var arr2 = ["http://placehold.it/200x200?text=敬请期待1","http://placehold.it/200x200?text=敬请期待2","http://placehold.it/200x200?text=敬请期待3"];
+    var author2 = ["Coming","Coming","Coming"];
 
 	var index2 = Math.floor((Math.random() * arr2.length));
     var link2 = arr2[index2];
+    var getauthor2 = author2[index2];
 	var finalm = "<img src='" + link2 + "' />"
+    var authortext = "<blockquote><small>骚图 by " + getauthor2 + "</small></blockquote>";
     document.getElementById("saoimg").innerHTML = finalm;
+    document.getElementById("saoauthor2").innerHTML = authortext;
 
 }
+
