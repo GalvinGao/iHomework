fbc6f74dd6f0968e8219a8a7c2aac4803134229e
GalvinG <galvingaoper@gmail.com>
2017-02-25 10:00:20 +0800

    See description. Add alloy lever debugger, modify
    Add alloy lever debugger, modify index.html (remove CloudMusic, change Dialog domain), bit move on at ihi.js.

--- js/ihi.js
+++ js/ihi.js
@@ -1,12 +1,12 @@
 window.onload = function () {
 
+//$.ajax({url:"https://api.github.com/repos/GalvinGao/iHomework/commits",async:false,crossDomain:true,success:function(data){var ihiver=data[0].commit.message;var ihiupdate=data[0].sha;document.getElementById("ihi-version").innerHTML=ihiver;document.getElementById("ihi-update").innerHTML=ihiupdate;}});
+
     //$.get("http://api.github.com/repos/GalvinGao/iHomework/commits", function(data){document.getElementById("ihi-commitsha").innerHTML=JSON.stringify(data[0].sha);});
 
     var ihiver = "v4.1.1-alpha.4";
     var ihiupdate = "2017-1-2 14:32 GMT+8";
 
-    document.getElementById("ihi-version").innerHTML = ihiver;
-    document.getElementById("ihi-update").innerHTML = ihiupdate;
     //document.getElementById("ihi-commitsha").innerHTML = ihisha;
 
 }
