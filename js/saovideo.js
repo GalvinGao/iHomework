c3fdd6dace83baec451d1271cb05128b9a89ae5c
Galvin Gao <2403901511@qq.com>
2017-03-18 22:16:08 +0800

    Change to new domain.

--- js/saovideo.js
+++ js/saovideo.js
@@ -1,8 +1,8 @@
 function saovideo() {
     document.getElementById("saovideo").innerHTML = "";
 
-    var vcover = ["http://ojb479lh2.bkt.clouddn.com/saovideo/IMG_1754.mp4-s", "http://ojb479lh2.bkt.clouddn.com/saovideo/IMG_1753.mp4-s", "http://ojb479lh2.bkt.clouddn.com/saovideo/IMG_1764.mp4?vframe/jpg/offset/20/w/360/h/640"];
-    var vlink = ["http://ihomework.top/videohandler.html?url=http://ojb479lh2.bkt.clouddn.com/saovideo/IMG_1754.mp4", "http://ihomework.top/videohandler.html?url=http://ojb479lh2.bkt.clouddn.com/saovideo/IMG_1753.mp4", "http://ihomework.top/videohandler.html?url=http://ojb479lh2.bkt.clouddn.com/saovideo/IMG_1764.mp4"];
+    var vcover = ["http://main.qn.iblueg.cn/saovideo/IMG_1754.mp4-s", "http://main.qn.iblueg.cn/saovideo/IMG_1753.mp4-s", "http://main.qn.iblueg.cn/saovideo/IMG_1764.mp4?vframe/jpg/offset/20/w/360/h/640"];
+    var vlink = ["http://ihomework.top/videohandler.html?url=http://main.qn.iblueg.cn/saovideo/IMG_1754.mp4", "http://ihomework.top/videohandler.html?url=http://main.qn.iblueg.cn/saovideo/IMG_1753.mp4", "http://ihomework.top/videohandler.html?url=http://main.qn.iblueg.cn/saovideo/IMG_1764.mp4"];
     var author3 = ["Authur", "Felix", "Authur and Teddy"];
 
     var index3 = Math.floor((Math.random() * vcover.length));
